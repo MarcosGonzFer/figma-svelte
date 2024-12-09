cfbf5b3c28b07cb0db54d79aae18c5ecb046cf0a
MarcosGonzFer <marcos0gon@gmail.com>
2024-12-09 09:12:15 +0100

    Realizamos el componente header

--- src/componentes/Header.svelte
+++ src/componentes/Header.svelte
@@ -1,58 +1,24 @@
-<script>
-  let firma = "./imagenes/firma.jpg";
-  let logoCarrito = './imagenes/logo-carrito.png'
-</script>
-
-<div class="contenedor-header">
-  <div class="Logo-marca">
-    <img src={firma} alt="La firma de la página" />
-    <h1>ZASHOP</h1>
-  </div>
-  <div class="header-nav">
-    <a href="#">Ofertas</a>
-    <a href="#">Tienda</a>
-    <a href="#">Ayuda</a>
-    <a href="#">Log-in</a>
-    
-  </div>
-  <button class="boton-carrito"> <img src={logoCarrito} alt="la imagen del carrito">CARRITO</button>
-</div>
-
 <style>
-  .contenedor-header {
-    display: flex;
-    flex-wrap: wrap;
-    color: #AD8A1F;
-    width: 100%;
+  header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1rem;
   }
-  
-  .Logo-marca{
-    display: flex;
-    flex-wrap: wrap;
-    color: #AD8A1F;
+  nav a {
+      margin: 0 0.5rem;
+      text-decoration: none;
+      color: #333;
   }
-  a{
-    text-decoration: none; 
-    color: #AD8A1F;
-    margin: 10px;
-  }
-  .header-nav{
-    margin-top: 35px;
-    margin-left: 500px;
-  }
-  .boton-carrito{
-    color: #AD8A1F;
-    font-family: League Gothic;
-    border-radius: 15px;
-    height: 30px;
-    border: 0.5px solid #AD8A1F ;
-    margin-top: 25px;
-    margin-left: 10px;
-  }
-  .boton-carrito img{
-    height: 20px;
-    margin-top: 5px;
-  }
-
-  
 </style>
+
+<header>
+  <h1>ZASHOP</h1>
+  <nav>
+      <a href="/">Ofertas</a>
+      <a href="/tienda">Tienda</a>
+      <a href="/ayuda">Ayuda</a>
+      <a href="/login">Log-in</a>
+      <a href="/carrito">🛒 Carrito</a>
+  </nav>
+</header>
